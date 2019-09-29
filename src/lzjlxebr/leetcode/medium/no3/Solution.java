be315abf7e1d4a6bfd60daa1ca2959cba4c702bb
lzjlxebr <tyyzmyd@gmail.com>
2019-09-29 22:37:07 +0800

    ...

---
+++ src/lzjlxebr/leetcode/medium/no3/Solution.java
@@ -0,0 +1,12 @@
+package lzjlxebr.leetcode.medium.no3;
+
+/**
+ * Solution
+ * <p>
+ * Description write in here.
+ *
+ * @author : lzjlxebr
+ * @date : 2019-09-29 22:35
+ **/
+public class Solution {
+}
